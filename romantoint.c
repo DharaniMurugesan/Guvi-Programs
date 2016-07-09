a8da024c20fcee63e8306098eb4f997039b76548
DharaniMurugesan <dharanimurugesan05@gmail.com>
2016-07-09 10:54:50 +0530

    Create romantoint.c

---
+++ romantoint.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter a year to convert it into roman numerical\n");
+scanf("%d",&a);
+roman(a);
+}
+roman(int year)
+{
+if(year>=1000)
+{
+printf("m");
+roman(year-1000);
+}
+else if(year>=500)
+{
+printf("d");
+roman(year-500);
+}
+else if(year>=100)
+{
+printf("c");
+roman(year-100);
+}
+else if(year>=50)
+{
+printf("l");
+roman(year-50);
+}
+else if(year>=10)
+{
+printf("x");
+roman(year-10);
+}
+else if(year>=5)
+{
+printf("v");
+roman(year-5);
+} 
+else if(year>=1)
+{
+printf("i");
+roman(year-1);
+}
+}
